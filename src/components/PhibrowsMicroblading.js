6078023b76678f8f70f7a22238ef540b1676343b
Heiko <heiko.eckmann@yahoo.com>
2020-08-10 19:35:31 +0200

    first push

---
+++ src/components/PhibrowsMicroblading.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './PhibrowsMicroblading.css'
+
+class PhibrowsMicroblading extends Component {
+
+    render()
+    {
+        return(
+            <div className="container">
+                <div className="jumbotron mt-5">
+                    <div className="col-sm-8 mx-auto">
+                    <center><h2 className = "a">PhiAcademy empfiehlt Royal Artists <br></br>und garantiert ihnen Qualität und <br></br> Erfahrung</h2></center>        
+                    </div>
+                    <section>
+                    <img src="/images/phibrowsmicroblading.jpg" alt=""></img>
+                    </section>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default PhibrowsMicroblading
