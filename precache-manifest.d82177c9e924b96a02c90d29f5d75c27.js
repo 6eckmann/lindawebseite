60cc341f9416cf95e598cb0ab870d05b11283230
Heiko <heiko.eckmann@yahoo.com>
2020-08-10 19:38:29 +0200

    Updates

---
+++ precache-manifest.d82177c9e924b96a02c90d29f5d75c27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099b374edeb114cdc02667ce1db44ffa",
+    "url": "/lindawebseite/index.html"
+  },
+  {
+    "revision": "014ee10ee527081a86fc",
+    "url": "/lindawebseite/static/css/main.1b694d7b.chunk.css"
+  },
+  {
+    "revision": "f925d28e70961b356e72",
+    "url": "/lindawebseite/static/js/2.b8e59d34.chunk.js"
+  },
+  {
+    "revision": "702e06c21f4db63e7e05a8a0eda9baf1",
+    "url": "/lindawebseite/static/js/2.b8e59d34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "014ee10ee527081a86fc",
+    "url": "/lindawebseite/static/js/main.55eda0e7.chunk.js"
+  },
+  {
+    "revision": "1f252a8ee9f4251c68fb",
+    "url": "/lindawebseite/static/js/runtime-main.9ea615d1.js"
+  }
+]);
